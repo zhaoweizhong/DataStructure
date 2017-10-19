0941489540827bf465162d20581a4c84e4841a3d
Zhaowei Zhong <zhong_zhaowei@163.com>
2017-10-19 15:00:18 +0800

    Update

--- src/lab3/QuestionC.java
+++ src/lab3/QuestionC.java
@@ -1,5 +1,70 @@
 package lab3;
 
-public class QuestionC {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class QuestionC {
+    private static ArrayList<Integer> group = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOfMessages = scanner.nextInt();
+        for (int t = 0; t < numOfMessages; t++) {
+            int numOfSoldiers = scanner.nextInt();
+            for (int i = 0; i < numOfSoldiers; i++) {
+                group.add(scanner.nextInt());
+            }
+            int numOfOperations = scanner.nextInt();
+            for (int i = 0; i < numOfOperations; i++) {
+                switch (scanner.nextInt()) {
+                    case 1: {
+                        int position = scanner.nextInt();
+                        int value = scanner.nextInt();
+                        insert(value, position);
+                        print();
+                        continue;
+                    }
+                    case 2: {
+                        int position = scanner.nextInt();
+                        delete(position);
+                        print();
+                        continue;
+                    }
+                    case 3: {
+                        int position = scanner.nextInt();
+                        int value = scanner.nextInt();
+                        update(value, position);
+                        print();
+                        continue;
+                    }
+                    case 4: reverse(); print();
+                }
+
+            }
+        }
+    }
+
+    public static void insert(int value, int position) {
+        group.add(position, value);
+    }
+
+    public static void delete(int position) {
+        group.remove(position);
+    }
+
+    public static void update(int value, int position) {
+        group.set(position, value);
+    }
+
+    public static void reverse() {
+        Collections.reverse(group);
+    }
+
+    public static void print() {
+        for (int num:group) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
